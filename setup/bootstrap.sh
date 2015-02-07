f3bffc0d141ae16ae68f3cd5968ea434b767892d
motleytech <motleytechnet@gmail.com>
2015-02-07 12:44:17 -0800

    changing bootstrap script

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -14,6 +14,7 @@ else
 fi
 
 # install aptitude
+echo -e "\n\nInstalling aptitude... \n\n"
 sudo apt-get install -y aptitude
 
 # update and upgrade aptitude
@@ -21,28 +22,37 @@ sudo aptitude -y update
 sudo aptitude -y upgrade
 
 # install git
+echo -e "\n\nInstalling git... \n\n"
 sudo aptitude install -y git
 
 # make folders and copy env file
+echo -e "\n\nCreating directories... \n\n"
 sudo mkdir -p $WEB_ROOT_FOLDER
 sudo mkdir -p $WEB_ROOT_FOLDER/envs
 sudo cp -n ~/envs/env_webcms.sh $WEB_ROOT_FOLDER/envs
 
+
+# clone git repo
+echo -e "\n\nCloning git repo... \n\n"
+cd $WEB_ROOT_FOLDER
+git clone $REPO_URL $WEB_ROOT_FOLDER/$REPO_NAME
+
+
+# execute the real installer script now
+echo -e "\n\nRunning the main install script now... \n\n"
+cd $REPO_NAME
+python setup/install.py
+
+
 # create user webuser and group webapps
+echo -e "\n\nCreating users and groups... \n\n"
 sudo groupadd -f --system $WEB_GROUP
 sudo useradd --system --gid $WEB_GROUP --shell /bin/bash --home $WEB_ROOT_FOLDER $WEB_USER
-
-# change the owner for $WEB_ROOT_FOLDER
-sudo chown -R $WEB_USER:$WEB_GROUP $WEB_ROOT_FOLDER
-sudo chmod -R g+w $WEB_ROOT_FOLDER
 
 # add current user to group users
 sudo usermod -a -G $WEB_GROUP `whoami`
 
-# clone git repo
-sudo su $WEB_USER -c "git clone $REPO_URL $WEB_ROOT_FOLDER/$REPO_NAME"
+# at the end... change permissions of folders
+sudo chown -R $WEB_USER:$WEB_GROUP $WEB_ROOT_FOLDER
+sudo chmod -R g+w $WEB_ROOT_FOLDER
 
-
-
-# execute the real installer script now
-sudo su $WEB_USER -c "cd $WEB_ROOT_FOLDER/$REPO_NAME; python setup/install.py"
