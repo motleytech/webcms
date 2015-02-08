fd4b79c9a1cf5069b26dbd65e325f3be2d48fa2d
motleytech <motleytechnet@gmail.com>
2015-02-08 12:47:50 -0800

    Apparently, we don't require a -e parameter for echo when using os.system('echo...')

--- djcms/setup_sites.py
+++ djcms/setup_sites.py
@@ -5,6 +5,6 @@ from django.contrib.sites.models import Site
 new_site = Site.objects.create(domain="motleytech.net", name="motleytech.net")
 
 print "\n\nSite Motleytech.net created with id : %s\n" % new_site.id
-os.system('echo -e "\nSITE_ID = %s" >> djcms/settings.py' % new_site.id)
+os.system('echo "\nSITE_ID = %s" >> djcms/settings.py' % new_site.id)
 
 print "New site id appended to settings."
