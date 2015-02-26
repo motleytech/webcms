a4c1976b93ffa30d7679c8f1ffb0270de4aa0318
motleytech <motleytechnet@gmail.com>
2015-02-25 17:05:59 -0800

    bootstrap_dev now asks before overwriting env_webcms.sh

--- setup/bootstrap_dev.py
+++ setup/bootstrap_dev.py
@@ -99,7 +99,12 @@ def main():
             run_command("rm -rf %s" % REPO_PATH)
         run_command("git clone %s" % REPO_URL)
 
-    run_command("cp %s/config/sample_env_webcms.sh %s/conf/env_webcms.sh" % (REPO_PATH, THIS_DIR))
+    envcms_exists = run_command("[ -d %s/conf/env_webcms.sh ]" % THIS_DIR, True, False)
+    if envcms_exists:
+        if confirm("env_webcms.sh already exists. Overwrite (yes, no)? "):
+            run_command("cp %s/config/sample_env_webcms.sh %s/conf/env_webcms.sh" % (REPO_PATH, THIS_DIR))
+    else:
+        run_command("cp %s/config/sample_env_webcms.sh %s/conf/env_webcms.sh" % (REPO_PATH, THIS_DIR))
 
     print """\
 
