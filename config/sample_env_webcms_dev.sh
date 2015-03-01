f175d66c6f1dc6d96f2b3c92f7bc340a6365cfd0
motleytech <motleytechnet@gmail.com>
2015-02-28 19:37:29 -0800

    adding new env_webcms files

---
+++ config/sample_env_webcms_dev.sh
@@ -0,0 +1,11 @@
+
+# make sure you change these values for a production server.
+export DJANGO_SECRET="any_random_string_will_work_here.Just_make_it_nice_and_long"
+export PG_USER_PW="sample_user_pw"
+export PG_ADMIN_PW="sample_admin_pw"
+
+# set this to "True" to enable django debug mode.
+# only set this to True in development mode
+# not recommended in production.
+export DJANGO_DEBUG="True"
+export PRODUCTION_ENV="False"
