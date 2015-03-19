6c615a287e63e909dfa5da23ff6eebc537f7df6d
motleytech <motleytechnet@gmail.com>
2015-03-18 23:55:32 -0700

    adding disqus shortname

--- setup/ws_settings.py
+++ setup/ws_settings.py
@@ -11,7 +11,7 @@ import os
 SITE_DETAILS = [
     # name(unique), domain, forwarding, num_django_processes
     ('motleytechnet', 'www.motleytech.net', '.motleytech.net', 3),
-    #('nagarajancom', 'www.nagarajan.com', '.nagarajan.com', 2),
+    #('nagarajancom', 'www.nagarajan.com', '.nagarajan.com', 1),
 
     # we can also serve multiple domains using the same server (different processes).
     #('mypersonalweb', 'www.mypersonalweb.com', '.mypersonalweb.com', 1),
@@ -27,7 +27,7 @@ PG_USER  = "webdbuser"
 PG_DB    = "webcmsdb"
 
 # set this to a valid disqus shortname, if you want to use disqus comments
-DISQUS_SHORTNAME = ""
+DISQUS_SHORTNAME = "motleytech"
 
 
 ###############################################################
